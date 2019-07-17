a8aa6640f340dcb0b237abe09b191b20df9de5e8
Sheila Ward <ssward1@gtcc.edu>
2019-07-17 18:45:36 -0400

    Initial commit

---
+++ SheilaWard_PortfolioMVC/Controllers/ExercisesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SheilaWard_PortfolioMVC.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        // GET: Exercises
+        public ActionResult JSBasics()
+        {
+            return View();
+        }
+        public ActionResult Tester()
+        {
+            return View();
+        }
+    }
+}
---
+++ SheilaWard_PortfolioMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SheilaWard_PortfolioMVC.MvcApplication" Language="C#" %>
